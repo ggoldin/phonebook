c7c65af8bced0189fd6589e0d88a7ab5136c3c79
ggoldin <goldin91@gmail.com>
2014-04-25 21:58:23 +0200

    removed directory db and php

--- db/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE config (key VARCHAR(50) PRIMARY KEY, value VARCHAR(255));
-CREATE TABLE entries (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname VARCHAR(255), lastname VARCHAR(255), phone text);
-
--- Since sqlite doesn't currently support foreign keys the following trigger is needed to make sqlite do the
--- equivalent of "ON DELETE CASCADE" on the task's "list_id" foreign key.
--- For more info see: http://www.sqlite.org/cvstrac/wiki?p=ForeignKeyTriggers
