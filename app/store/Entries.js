7778b01ed07ece922ef1e6c23049eb143d9f1ab1
ggoldin <goldin91@gmail.com>
2014-04-26 22:07:14 +0200

    remoteFilter: true removed

--- app/store/Entries.js
+++ app/store/Entries.js
@@ -1,5 +1,6 @@
 Ext.define('Phonebook.store.Entries', {
     extend: 'Ext.data.Store',
     model: 'Phonebook.model.Entries',
-    autoLoad: true,        remoteFilter: true
+    autoLoad: true
+    //remoteFilter: true     
 });
