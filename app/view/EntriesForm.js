0040c02474f75bb526c70f54e5e5d2820d7995e4
ggoldin <goldin91@gmail.com>
2014-04-28 19:09:13 +0200

    Update EntriesForm.js
    Add property: maxLength: 255 and enforceMaxLength: true

--- app/view/EntriesForm.js
+++ app/view/EntriesForm.js
@@ -16,7 +16,7 @@ Ext.define('Phonebook.view.EntriesForm', {
         margin: '10 5 5 5',
         layout: 'fit',
         border: 'none',
-        defaults: {xtype: 'textfield', allowBlank: false, margin: '0 0 5 0', afterLabelTextTpl: '<span style="color:red;font-weight:bold" data-qtip="Required">*</span>'},
+        defaults: {xtype: 'textfield', allowBlank: false, margin: '0 0 5 0', afterLabelTextTpl: '<span style="color:red;font-weight:bold" data-qtip="Required">*</span>', maxLength: 255, enforceMaxLength: true},
         items: [{
             name: 'firstname',
             fieldLabel: 'First name',
