2f5e78479cd0476e82bf6b54aa0779f7ac8789a8
ggoldin <goldin91@gmail.com>
2014-04-28 18:50:01 +0200

    Update createEntriesTable.sql

--- createEntriesTable.sql
+++ createEntriesTable.sql
@@ -4,8 +4,8 @@
 
 CREATE TABLE IF NOT EXISTS `entries` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `firstname` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `lastname` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `phone` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `firstname` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `lastname` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
